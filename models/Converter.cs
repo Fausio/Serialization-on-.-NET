1f4f4c3edefb2cd1ab62f318973f306974d13269
fausio luis <luisfausio@gmail.com>
2021-01-07 15:37:03 +0200

    QueryObject model

--- models/Converter.cs
+++ models/Converter.cs
@@ -1,14 +1,25 @@
+using System;
+using System.Data;
+
 namespace Serialization_on_._NET.models
 {
     public class Converter
     {
         public static void Run()
         {
-
-            //Serialize();
+            Serialize();
             //DeSerialize();
             //Anonymouse();
+        }
 
+        public static void Serialize()
+        {
+            int id = new Random().Next(0, 100);
+
+            var  myQuery = new QueryObject
+            {
+
+            }
         }
     }
 }
---
+++ models/QueryObject.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Serialization_on_._NET.models
+{
+    public class QueryObject
+    {
+        public string Text { get; set; }
+        public TimeSpan TimesTamp { get; set; }
+        public string Url { get; set; }
+        public int Id { get; set; }
+
+    }
+}
